41bd63e5376d985dc99f5e0be3586d959e7c8661
Oliver Ong <tefiri@gmail.com>
2014-09-16 07:20:00 +0800

    added event handling

--- src/basic/js/bvs.js
+++ src/basic/js/bvs.js
@@ -2,30 +2,54 @@
 
     $.fn.bvs = function( options ) {
 
-        // This is the easiest way to have default options.
+        // DEFAULT OPTIONS
         var settings = $.extend({
             // These are the defaults.
+            // These are the events
+            onComplete: false,
+            onStart: false,
+            onHoverOut: false
         }, options );
 
         var bvs = this;
 
-        $(this).hover(
+        // TRIGGER EVENTS
+        bvs.triggerEvent = function(event){
+            if(event == "start-complete"){
+                // trigger start and complete
+                if(settings.onStart != false){
+                    settings.onStart(this);
+                }
+                setTimeout(function(){
+                    if(settings.onComplete != false){
+                        settings.onComplete(this);
+                    }
+                },400);
+            }
+            else if(event == "hover-out"){
+                // trigger hover out
+                if(settings.onHoverOut != false){
+                    settings.onHoverOut(this);
+                }
+            }
+        };
+
+        // ANIMATION EXECUTION AND TRIGGER EVENTS
+        $(bvs).hover(
             function(){
                 bvs.removeClass("active");
                 bvs.addClass("notactive");
                 $(this).addClass("active");
                 $(this).removeClass("notactive");
+                bvs.triggerEvent("start-complete");
             },function(){
                 bvs.removeClass("active");
                 bvs.removeClass("notactive");
+                bvs.triggerEvent("hover-out");
             }
         );
 
-        // Greenify the collection based on the settings variable.
         return bvs;
 
     };
 }( jQuery ));
-/**
- * Created by bingo on 9/15/14.
- */
